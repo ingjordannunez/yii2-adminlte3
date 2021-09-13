7e7e089deb503d8671bdd15c464835a5c0be17d9
ingjordannunez <68974138+ingjordannunez@users.noreply.github.com>
2021-09-13 17:09:50 -0500

    Update main.php

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -5,8 +5,8 @@
 
 use yii\helpers\Html;
 
-\hail812\adminlte3\assets\FontAwesomeAsset::register($this);
-\hail812\adminlte3\assets\AdminLteAsset::register($this);
+\ingjordannunez\adminlte3\assets\FontAwesomeAsset::register($this);
+\ingjordannunez\adminlte3\assets\AdminLteAsset::register($this);
 $this->registerCssFile('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700');
 
 $assetDir = Yii::$app->assetManager->getPublishedUrl('@vendor/almasaeed2010/adminlte/dist');
